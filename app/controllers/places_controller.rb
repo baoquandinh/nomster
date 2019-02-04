348ee880ebbc4076e8ab1ddb576293181e8c576c
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-04 06:45:01 +0000

    Added permission checks for edit, update, and delete function. Only the user who created the specific place may edit or destory it.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,10 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
         # @places = Place.all
         # Notes on how to use will_paginate
-        @places = Place.all.paginate(:page => params[:page], :per_page => 2)
+        @places = Place.all.paginate(:page => params[:page], :per_page => 5)
     end
 
     def new
@@ -22,16 +22,26 @@ class PlacesController < ApplicationController
 
     def edit
         @place = Place.find(params[:id])
+
+        if current_user != @place.user
+            return render plain: "Not Allowed", status: :forbidden
+        end
     end
 
     def update
         @place = Place.find(params[:id])
+        if current_user != @place.user
+            return render plain: "Not Allowed", status: :forbidden
+        end
         @place.update_attributes(place_params)
         redirect_to root_path
     end
 
     def destroy
         @place = Place.find(params[:id])
+        if current_user != @place.user
+            return render plain: "Not Allowed", status: :forbidden
+        end
         @place.destroy
         redirect_to root_path
     end
