2995e2641698ac6900fa5fde456237522b8db725
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-09 19:07:31 +0000

    Created comments section to list all in the data for specific page. Changed ratings to dropdown instead of text input

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+
+    RATINGS = {
+        'one_star': "1_stars",
+        'two_star': "2_stars",
+        'three_star': "3_stars",
+        'four_star': "4_stars",
+        'five_star': "5_stars",
+    }
 end
