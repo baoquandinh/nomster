41bf9aa61386f1f13e821759efd6f69bf9e9044c
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-22 08:03:08 +0000

    Removed photo db

--- db/migrate/20190209221925_create_photos.rb
+++ db/migrate/20190209221925_create_photos.rb
@@ -1,9 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
-    create_table :photos do |t|
-      t.text :caption
-      t.integer :place_id
-      t.timestamps
-    end
+    # create_table :photos do |t|
+    #   t.text :caption
+    #   t.integer :place_id
+    #   t.timestamps
+    # end
   end
 end
